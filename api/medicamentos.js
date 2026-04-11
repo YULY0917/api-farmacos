d4f47ac4c86aeefb6e803409aba1dcb4134454a4
YULY0917 <yuly.trujillo0318@gmail.com>
2026-04-11 17:19:45 -0400

    Update medicamentos.js

--- api/medicamentos.js
+++ api/medicamentos.js
@@ -1,7 +1,12 @@
-import productos from '../data/productos.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
 
 export default function handler(req, res) {
   try {
+    const filePath = path.join(process.cwd(), 'data', 'productos.json');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const productos = JSON.parse(fileContents);
+
     return res.status(200).json(productos);
   } catch (error) {
     return res.status(500).json({
