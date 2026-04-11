9ba2064d5453bf53af40851280f51c088fc2c5da
YULY0917 <yuly.trujillo0318@gmail.com>
2026-04-11 16:45:53 -0400

    Update medicamentos.js

--- api/medicamentos.js
+++ api/medicamentos.js
@@ -1,1 +1,14 @@
-
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      nombre: "Paracetamol",
+      precioTexto: "$1000",
+      fuente: "CENABAST"
+    },
+    {
+      nombre: "Ibuprofeno",
+      precioTexto: "$1500",
+      fuente: "CENABAST"
+    }
+  ]);
+}
